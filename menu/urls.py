03e9260633054a3abe042ba26440cc4971749a1c
Kyryl <sautakiki.com@gmail.com>
2026-04-17 19:26:01 +0300

    updated menu/urls.py & menu/views.p

--- menu/urls.py
+++ menu/urls.py
@@ -1,24 +1,43 @@
 # menu/urls.py
 from django.urls import path
-from .views import index # or whatever views you have
+from .views import (index,
+                    DishListView,
+                    DishCreateView,
+                    DishUpdateView,
+                    DishDetailView,
+                    DishTypeListView,
+                    DishDeleteView,
+                    DishTypeDetailView,
+                    DishTypeCreateView,
+                    DishTypeUpdateView,
+                    DishTypeDeleteView,
+                    CookListView,
+                    CookDetailView,
+                    CookCreateView,
+                    CookUpdateView,
+                    CookDeleteView
+                    )
 
-app_name = "menu"  # <--- ADD THIS LINE
+app_name = "menu"
 
 urlpatterns = [
     path("", index, name="index"),
     #Dish Views
-    path("dishes/", DishListView.as_view(), name="dishes-list"),
-    path("dishes/create/",DishCreateView.as_view(), name="dish-create"),
-    path("dishes/<int:pk>/update", DishUpdateView.as_view(), name="dish-update"),
-    path("dishes/<int:pk>/delete", DishDeleteView.as_view(), name="dish-delete"),
+    path("dish/", DishListView.as_view(), name="dish-list"),
+    path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dish/create/",DishCreateView.as_view(), name="dish-create"),
+    path("dish/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
+    path("dish/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
     #Dishtype views
-    path("dish-types/", DishTypeListView.as_view(), name="dish-types-list"),
-    path("dish-types/create/",DishTypeCreateView.as_view(), name="dish-type-create"),
-    path("dish-types/<int:pk>/update", DishTypeUpdateView.as_view(), name="dish-type-update"),
-    path("dish-types/<int:pk>/delete", DishTypeDeleteView.as_view(), name="dish-type-delete"),
+    path("dish-type/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish-type/<int:pk>/", DishTypeDetailView.as_view(), name="dish-type-detail"),
+    path("dish-type/create/",DishTypeCreateView.as_view(), name="dish-type-create"),
+    path("dish-type/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path("dish-type/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
     #Cook views
-    path("cooks/", CookListView.as_view(), name="cooks-list"),
-    path("cooks/create/",CookCreateView.as_view(), name="cook-create"),
-    path("cooks/<int:pk>/update", CookUpdateView.as_view(), name="cook-update"),
-    path("cooks/<int:pk>/delete", CookDeleteView.as_view(), name="cook-delete"),
+    path("cook/", CookListView.as_view(), name="cook-list"),
+    path("cook/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
+    path("cook/create/",CookCreateView.as_view(), name="cook-create"),
+    path("cook/<int:pk>/update/", CookUpdateView.as_view(), name="cook-update"),
+    path("cook/<int:pk>/delete/", CookDeleteView.as_view(), name="cook-delete"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -21,10 +21,93 @@ def index(request):
         "num_dish_types" : num_dish_types,
         "num_cooks": num_cooks,
     }
-    return render(request, "menu:index.html", context=context)
+    return render(request, "menu/index.html", context=context)
 
-
+#Dish views
 class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     context_object_name = "dish_list"
-    template_name =
+    template_name = "menu/dish_list.html"
+    paginate_by = 5
+
+
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Dish
+    template_name = "menu/dish_detail.html"
+
+
+class DishCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Dish
+    fields = "__all__"
+    success_url = reverse_lazy("menu:dish-list")
+
+
+class DishUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Dish
+    fields = "__all__"
+    success_url = reverse_lazy("menu:dish-list")
+
+
+class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Dish
+    success_url = reverse_lazy("menu:dish-list")
+
+#DishType views
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    context_object_name = "dish-type_list"
+    template_name = "menu/dishtype-list.html"
+    paginate_by = 5
+
+
+class DishTypeDetailView(LoginRequiredMixin, generic.DetailView):
+    model = DishType
+    template_name = "menu/dishtype-list.html"
+
+
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("menu:dish-type-list")
+
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("menu:dish-type-list")
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("menu:dish-type-list")
+
+
+#Cook views
+class CookListView(LoginRequiredMixin, generic.ListView):
+    model = Cook
+    context_object_name = "cook_list"
+    template_name = "menu/cook_list.html"
+    paginate_by = 5
+
+
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Cook
+    template_name = "menu/cook_detail.html"
+
+
+class CookCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Cook
+    fields = "__all__"
+    success_url = reverse_lazy("menu:cook-list")
+
+
+class CookUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Cook
+    fields = "__all__"
+    success_url = reverse_lazy("menu:cook-list")
+
+
+class CookDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Cook
+    success_url = reverse_lazy("menu:cook-list")
+
