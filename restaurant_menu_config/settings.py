a1fd73efc36a1a402bd5d45c0431877184e44ec6
Kyryl <sautakiki.com@gmail.com>
2026-04-18 19:04:40 +0300

    added logout_redirect_page

--- restaurant_menu_config/settings.py
+++ restaurant_menu_config/settings.py
@@ -117,7 +117,7 @@ USE_I18N = True
 USE_TZ = True
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login'
 
 # settings.py
 # This maps the boilerplate variable to your static path
